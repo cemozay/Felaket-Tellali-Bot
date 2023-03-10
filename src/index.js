898dc56785bb5dce9a07718bb353b935ef8c3a8f
Yunus Cem Özay <78884331+cemozay@users.noreply.github.com>
2023-03-10 18:45:58 +0300

    Earthquake api fetch and added commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+//Get required packages discord and dotenv
 require('dotenv').config();
 const { Client, IntentsBitField} = require('discord.js');
 
+//Set Client intents
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -10,19 +12,32 @@ const client = new Client({
     ],
 });
 
+//Is bot alive?
 client.on('ready', (c) => {
     console.log(`${c.user.username} is online`);
 });
 
+//Bot responses to slash commands
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'vamanos') {
-        interaction.reply('ALEEEEEEEEEEEEEEEEEEE!');
+    if (interaction.commandName === 'hi') {
+        interaction.reply(`Merhaba!`);
     }
-    if (interaction.commandName === 'who-is-ready-to-fly') {
-        interaction.reply(`I AM ${interaction.member.nickname}`);
+
+    if (interaction.commandName === 'deprem') {
+        interaction.reply(`${interaction.member.nickname} istekte bulundu.`);
     }
 });
+
+// Make a GET request to the API endpoint
+fetch('https://api.orhanaydogdu.com.tr/deprem/kandilli/live')
+    // Parse the response as JSON
+    .then(response => response.json())
+    // Log the parsed data to the console
+    .then(data => console.log(data))
+    // Log any errors to the console
+    .catch(error => console.error(error));
+
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -3,28 +3,28 @@ const { REST, Routes } = require('discord.js');
 
 const commands = [
     {
-        name: 'vamanos',
-        description: 'ALEEEE',
+        name: 'hi',
+        description: 'Say hi',
     },
     {
-        name: 'who-is-ready-to-fly',
-        description: 'I AM',
+        name: 'deprem',
+        description: 'Deprem verilerini getir.',
     },
 ];
 
 const rest = new REST ({ version: '10' }).setToken(process.env.TOKEN);
 
+//Upload slash commands to bot
 (async () => {
     try {
-        console.log('Slash komutları yukleniyor');
+        console.log('Uploading slash commands');
 
         await rest.put(
             Routes.applicationCommands(
-                process.env.CLIENT_ID, 
-                process.env.GUILD_ID),
+                process.env.CLIENT_ID),
             {body: commands}
         )
-        console.log('Slash komutları yuklendi');
+        console.log('Slash commands uploaded');
     } catch (error) {
         console.log(`There was an error: ${error}`);
     }
